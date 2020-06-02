54f4d0a90456af1aebac7a2e6506f554b54ad93a
Giulia Cajati <giuliacajati@gmail.com>
2020-06-02 19:38:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,5 +123,5 @@ def adding_to_dennis
     }
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages][1] =
 end
