0bab9ef0d510c19d18aeae4a537335d0211febe7
Giulia Cajati <giuliacajati@gmail.com>
2020-06-02 19:23:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,6 +88,8 @@ def changing_alan
   programmer_hash[:alan_kay][:known_for] = "GUI"
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
+  programmer_hash
+end 
 
 	programmer_hash = 
  		{
